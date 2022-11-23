27a86f2b5f12fbedacaf63587787abfda09cf76f
Aykuayvenchorr <nastyashevchenkomail@mail.ru>
2022-11-23 16:40:53 +0300

    test selection does not work

--- tests/selection/selection_create_test.py
+++ tests/selection/selection_create_test.py
@@ -4,9 +4,10 @@ from tests.factories import AdFactory
 
 
 @pytest.mark.django_db
-def test_selection_create(client, user, ad, access_token):
+def test_selection_create(client, user, access_token):
     ads = AdFactory.create_batch(5)
-    response = client.post("/selection/", {"name": "test selection", "owner": user.pk, "items": [ad.pk for ad in ads]},
+    response = client.post("/selection/", {"name": "test selection", "owner": user.pk,
+                                           "items": [ad.pk for ad in ads]},
                            HTTP_AUTHORIZATION="Bearer " + access_token)
     assert response.status_code == 201
     assert response.data == {"id": 1, "name": "test selection", "owner": user.pk, "items": [ad.pk for ad in ads]}
